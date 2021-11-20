2d213aedac310b63bc992f61340e44da729234de
Nermin Dedovic <dedovicnermin@outlook.com>
2021-11-19 19:53:26 -0600

    tests

--- src/test/java/tech/depaul/digitalatm/controllers/DigitalATMControllerTest.java
+++ src/test/java/tech/depaul/digitalatm/controllers/DigitalATMControllerTest.java
@@ -1,43 +1,118 @@
 package tech.depaul.digitalatm.controllers;
 
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithSecurityContext;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.view.RedirectView;
+import tech.depaul.digitalatm.TestUtils;
+import tech.depaul.digitalatm.config.ATMUserDetails;
+import tech.depaul.digitalatm.controllers.request.DigitalATMRequest;
 import tech.depaul.digitalatm.service.DigitalATMService;
 
-import java.net.URI;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
-@WebMvcTest(DigitalATMController.class)
 @ExtendWith(MockitoExtension.class)
 class DigitalATMControllerTest {
 
-    @Autowired private MockMvc mvc;
-    @MockBean private DigitalATMService atmService;
+    private static final String HOME = "home";
+    private static final String DEPOSIT = "deposit";
+    private static final String WITHDRAW = "withdraw";
+    private static final String BALANCE = "balance";
+    private static final String SUCCESS = "success";
 
 
 
-    @WithMockUser(value="spring")
-    @Test
-    void givenAuthRequestOnValidUser_shouldSucceedWithRedirect() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get(new URI("/")).contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(302));
+    @Mock private DigitalATMService mockAtmService;
+    @Mock private SecurityService mockSecurityService;
+
+    private Model model;
+    private DigitalATMController controller;
+
+    @BeforeEach
+    void setModel() {
+        model = new ExtendedModelMap();
+        controller = new DigitalATMController(mockAtmService, mockSecurityService);
     }
 
-    @WithUserDetails(userDetailsServiceBeanName = "")
+
     @Test
-    void onGetHome_withValidUser_returnsOK() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get(new URI("/home"))).andExpect(status().isOk());
+    void whenRedirectToHome_returnsCorrectRedirect() {
+        final RedirectView actual = controller.redirectToHome();
+        assertThat(actual).isNotNull();
+        assertThat(actual.getUrl()).isEqualTo("/home");
     }
+
+    @Test
+    void whenRedirectedToHome_withValidUser_returnsHomeViewWithModelPopulated() {
+        when(mockSecurityService.getCurrentUser()).thenReturn(TestUtils.getTestUserDetails());
+        final String actual = controller.home(model);
+        assertThat(model.getAttribute("username")).isEqualTo("TEST");
+        assertThat(actual).isEqualTo(HOME);
+
+    }
+
+    @Test
+    void whenUserRetrievesDepositPage_controllerReturnsCorrectView() {
+        assertThat(controller.getDeposit(model)).isEqualTo(DEPOSIT);
+    }
+
+    @Test
+    void whenUserSubmitsDeposit_withValidRequest_ThenReturnsSuccess() {
+        final DigitalATMController controller = new DigitalATMController(mockAtmService, mockSecurityService);
+        final ATMUserDetails testUserDetails = TestUtils.getTestUserDetails();
+        final DigitalATMRequest atmRequest = new DigitalATMRequest("10.00");
+
+        when(mockSecurityService.getCurrentUser()).thenReturn(testUserDetails);
+        doNothing().when(mockAtmService).executeDepositOnAccount(atmRequest,testUserDetails);
+
+        final String actual = controller.deposit(atmRequest, model);
+        assertThat(actual).isEqualTo(SUCCESS);
+        assertThat(model.getAttribute("message")).isEqualTo("DEPOSIT $10.00 from TEST's account was successful");
+    }
+
+    @Test
+    void whenValidUserDirectsToWithdraw_controllerReturnsCorrectView() {
+        assertThat(controller.getWithdraw(model)).isEqualTo(WITHDRAW);
+        assertThat(model.getAttribute("digitalAtmRequest")).isEqualTo(new DigitalATMRequest(null));
+
+    }
+
+    @Test
+    void whenValidUserDirectsToWithDraw_andPostsValidRequest_controllerReturnsCorrectView() {
+        final DigitalATMRequest digitalATMRequest = new DigitalATMRequest("10.00");
+        final ATMUserDetails testUserDetails = TestUtils.getTestUserDetails();
+
+        when(mockSecurityService.getCurrentUser()).thenReturn(testUserDetails);
+        doNothing().when(mockAtmService).executeWithdrawOnAccount(digitalATMRequest,testUserDetails);
+
+        final String actual = controller.withdraw(digitalATMRequest, model);
+        assertThat(actual).isEqualTo(SUCCESS);
+        assertThat(model.getAttribute("message")).isEqualTo("WITHDRAW $10.00 from TEST's account was successful");
+    }
+
+    @Test
+    void whenValidUserDirectsToViewBalance_isPresentedCorrectViewWithBalance() {
+        final ATMUserDetails testUserDetails = TestUtils.getTestUserDetails();
+        final String expectedBalance = "100.00";
+
+        when(mockSecurityService.getCurrentUser()).thenReturn(testUserDetails);
+        when(mockAtmService.retrieveAccountBalance(testUserDetails)).thenReturn(expectedBalance);
+
+        final String actual = controller.getBalance(model);
+        assertThat(actual).isEqualTo(BALANCE);
+        assertThat(model.getAttribute("balance")).isEqualTo(expectedBalance);
+        assertThat(model.getAttribute("username")).isEqualTo(testUserDetails.getUsername());
+
+    }
+
+
+
 
 
 }
